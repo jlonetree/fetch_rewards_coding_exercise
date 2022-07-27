d3a809dc04623191bf2694c7e40f025ba0e39db8
jlonetree <jlonetree@gmail.com>
2022-07-27 15:01:56 -0500

    added models for payer, transaction, and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'transaction#add_transaction'
-  get 'points_balance', to 'payer#points_balance'
-  get 'spend', to 'user#spend'
+  get 'points_balance', to: 'payer#points_balance'
+  get 'spend', to: 'user#spend'
 
 end
