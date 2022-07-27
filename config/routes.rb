c22aa2c1f75c88f7952e9d601097a0e4d0a0428a
jlonetree <jlonetree@gmail.com>
2022-07-27 14:38:19 -0500

    added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'transaction#add_transaction'
+  get 'points_balance', to 'payer#points_balance'
+  get 'spend', to 'user#spend'
+
 end
