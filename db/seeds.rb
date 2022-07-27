e10043e89003072f42b82ccbc35406ea3f64c41f
jlonetree <jlonetree@gmail.com>
2022-07-27 00:44:14 -0500

    added user and transaction seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Transaction.create([
+    { "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" },
+    { "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" },
+    { "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" },
+    { "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" },
+    { "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }
+])
+
+User.create(points: 5000)
