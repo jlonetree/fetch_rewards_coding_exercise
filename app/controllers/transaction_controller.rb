85d88c3d44dd2728b84f77568c1fd5e5d92005d5
jlonetree <jlonetree@gmail.com>
2022-07-27 17:26:06 -0500

    transaction_controller completed

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,5 +1,11 @@
 class TransactionController < ApplicationController
     def add_transaction
-        
+        Transaction.create(transaction_params)
+    end
+
+    private
+
+    def transaction_params
+       params.require(:transaction).permit(:payer, :points, :timestamp) 
     end
 end
