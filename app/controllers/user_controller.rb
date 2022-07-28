10cd6bc52f665a53096ae858dd34eda65ddd7bb1
jlonetree <jlonetree@gmail.com>
2022-07-28 13:32:03 -0500

    added points_balance function

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,26 +1,73 @@
+$payers = Payer.all
 class UserController < ApplicationController
+
     def spend
         user = User.find_by(params[:id])
         transactions = Transaction.all.sort_by{ | transaction | transaction[:timestamp] }
-        transaction_total = 0
+        spend_total = dannon_points = miller_coors_points = unilever_points = 0
         
         transactions.each do | transaction |
             payer = Payer.find_by(payer: transaction[:payer])
-            if user[:points] > transaction_total
-                transaction_total += transaction[:points]
-                if transaction[:payer] == payer[:payer]
-                    payer[:points] -= transaction[:points] 
-                    print "transaction_total #{transaction_total}\n"
-                    print "answer #{payer[:points]} \n payer #{payer[:payer]} transaction #{transaction[:payer]} \n\n"
+            payer_total = payer[:points]
+
+            if user[:points] >= spend_total
+                spend_total += transaction[:points]
+                user[:points] < spend_total ? excess_points = spend_total - user[:points] : excess_points = 0
+
+                if payer[:payer] == "DANNON"
+                    dannon_points = dannon_points - (transaction[:points] - excess_points)
+                elsif payer[:payer] == "MILLER COORS"
+                    miller_coors_points = miller_coors_points - (transaction[:points] - excess_points)
+                else
+                    unilever_points = unilever_points - (transaction[:points] - excess_points)
                 end
-            elsif transaction[:payer] == payer[:payer] && user[:points] < transaction_total
-                final_amount = transaction_total - (transaction_total - user[:points])
-                print "final_amount #{final_amount}\n transaction_total #{transaction_total}\n"
-                payer[:points] = (payer[:points] - transaction[:points])
-                print "else answer #{payer[:points]} \n payer #{payer[:payer]} transaction #{transaction[:payer]} \n\n"
             end
         end
 
-        render :json => Payer.all
+        payers = $payers.each{ | payer | 
+            if payer[:payer] == "DANNON"
+                payer[:points] = dannon_points
+            elsif payer[:payer] == "MILLER COORS"
+                payer[:points] = miller_coors_points
+            else
+                payer[:points] = unilever_points
+            end
+        }
+
+        render :json => $payers, :except => [:id, :created_at, :updated_at]
+        return $payers
     end
+
+    def points_balance
+        transactions = Transaction.all
+        payer_balances = {}
+        dannon_points = miller_coors_points = unilever_points = 0
+
+        transactions.each do | transaction |
+            payer = Payer.find_by(payer: transaction[:payer])
+            if payer[:payer] == "DANNON"
+                dannon_points = dannon_points - transaction[:points]
+            elsif payer[:payer] == "MILLER COORS"
+                miller_coors_points = miller_coors_points - transaction[:points]
+            else
+                unilever_points = unilever_points - transaction[:points]
+            end 
+        end
+
+        $payers.each { | payer | 
+            if payer[:payer] == "DANNON"
+                payer[:points] -= dannon_points
+                payer_balances.store(payer[:payer], payer[:points]) 
+            elsif payer[:payer] == "MILLER COORS"
+                payer[:points] -= miller_coors_points
+                payer_balances.store(payer[:payer], payer[:points]) 
+            else
+                payer[:points] -= unilever_points
+                payer_balances.store(payer[:payer], payer[:points])
+            end
+        }
+
+        render :json => payer_balances
+    end
+
 end
