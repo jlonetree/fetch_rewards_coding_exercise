d380b867b670ddf21137d9ca6f33241fe2908296
jlonetree <jlonetree@gmail.com>
2022-07-28 13:33:19 -0500

    removed the points_balance function

--- app/controllers/payer_controller.rb
+++ app/controllers/payer_controller.rb
@@ -1,9 +1,2 @@
 class PayerController < ApplicationController
-    def points_balance
-        payer_balances = {}
-
-        Payer.all.each { | payer | payer_balances.store(payer[:payer], payer[:points]) }
-
-        render :json => payer_balances
-    end
 end
