ef3ef696bb988ebb436a5b1507778e7a72725ad1
jlonetree <jlonetree@gmail.com>
2022-07-27 23:52:40 -0500

    second draft of user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,25 +1,26 @@
 class UserController < ApplicationController
     def spend
-        points = 5000
-        payer = Payer.all
-        transactions = Transaction.all
-        transaction_total, payer_points_total = 0
-
-        transactions = transactions.sort_by{ | transaction | transaction[:timestamp] }
-
-        transactions.map do | transaction |
-            while points > transaction_total
+        user = User.find_by(params[:id])
+        transactions = Transaction.all.sort_by{ | transaction | transaction[:timestamp] }
+        transaction_total = 0
+        
+        transactions.each do | transaction |
+            payer = Payer.find_by(payer: transaction[:payer])
+            if user[:points] > transaction_total
                 transaction_total += transaction[:points]
-                if transaction[:payer] == payer[:payer] && transaction_total < points
-                    payer[:points] -= transaction[:points]
-                else
-                    final_amount = transaction_total - points
-                    payer[:points] = payer[:points] - transaction[:points] + final_amount
+                if transaction[:payer] == payer[:payer]
+                    payer[:points] -= transaction[:points] 
+                    print "transaction_total #{transaction_total}\n"
+                    print "answer #{payer[:points]} \n payer #{payer[:payer]} transaction #{transaction[:payer]} \n\n"
                 end
+            elsif transaction[:payer] == payer[:payer] && user[:points] < transaction_total
+                final_amount = transaction_total - (transaction_total - user[:points])
+                print "final_amount #{final_amount}\n transaction_total #{transaction_total}\n"
+                payer[:points] = (payer[:points] - transaction[:points])
+                print "else answer #{payer[:points]} \n payer #{payer[:payer]} transaction #{transaction[:payer]} \n\n"
             end
         end
-        # points spent must be lower than transaction total
-        # points will be displayed as negative as the user is spending those payer's points
-        # i.e. { payer: foo, points: -100 } aka: add to payer array of hashes
+
+        render :json => Payer.all
     end
 end
