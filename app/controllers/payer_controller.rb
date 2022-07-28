69d7ce4080446d91d45128f73e2846d0dbd60caf
jlonetree <jlonetree@gmail.com>
2022-07-28 00:48:22 -0500

    added logic with correct output style

--- app/controllers/payer_controller.rb
+++ app/controllers/payer_controller.rb
@@ -2,6 +2,8 @@ class PayerController < ApplicationController
     def points_balance
         payer_balances = {}
 
+        Payer.all.each { | payer | payer_balances.store(payer[:payer], payer[:points]) }
+
         render :json => payer_balances
     end
 end
